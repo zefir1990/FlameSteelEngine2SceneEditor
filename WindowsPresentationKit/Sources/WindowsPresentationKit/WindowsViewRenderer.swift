8ee60cd400f9433f1e92fb619abadae5955154b0
Ilia Prokhorov <demensdeum@gmail.com>
2026-04-02 11:54:59 +0400

    Prototyping 17

--- WindowsPresentationKit/Sources/WindowsPresentationKit/WindowsViewRenderer.swift
+++ WindowsPresentationKit/Sources/WindowsPresentationKit/WindowsViewRenderer.swift
@@ -1,38 +1,53 @@
 import PresentationKit
 
-public struct WindowsViewRenderer: ViewRenderer {
-    public init(parent: (any View)?) {}
+public class WindowsViewRenderer: ViewRenderer {
+    private var depth = 0
+    private func indent() -> String {
+        return String(repeating: "  ", count: depth)
+    }
+
+    public required init(parent: (any View)?) {}
 
     public func render(_ view: any View) {
-        print("\(type(of: self)): Rendering view \(type(of: view))")
+        depth = 0
+        print("\(indent())\(type(of: self)): Rendering view \(type(of: view))")
         view.accept(self)
     }
 
     public func visit(_ button: Button) {
-        print("\(type(of: self)): visit Button")
+        print("\(indent())\(type(of: self)): visit Button")
+        depth += 1
+        button.label.accept(self)
+        depth -= 1
     }
 
     public func visit(_ text: Text) {
-        print("\(type(of: self)): visit Text - \(text.content)")
+        print("\(indent())\(type(of: self)): visit Text - \(text.content)")
     }
 
     public func visit(_ panel: Panel) {
-        print("\(type(of: self)): visit Panel - \(panel.children.count) children")
+        print("\(indent())\(type(of: self)): visit Panel - \(panel.children.count) children")
+        depth += 1
         for child in panel.children {
             child.accept(self)
         }
+        depth -= 1
     }
 
     public func visit(_ viewGroup: ViewGroup) {
-        print("\(type(of: self)): visit ViewGroup - \(viewGroup.views.count) views")
+        print("\(indent())\(type(of: self)): visit ViewGroup - \(viewGroup.views.count) views")
+        depth += 1
         for view in viewGroup.views {
             view.accept(self)
         }
+        depth -= 1
     }
 
     public func visit(_ modifiedView: ModifiedView) {
-        print("\(type(of: self)): visit ModifiedView - size: \(modifiedView.size)")
+        print("\(indent())\(type(of: self)): visit ModifiedView - size: \(modifiedView.size)")
+        depth += 1
         modifiedView._content.accept(self)
+        depth -= 1
     }
 
     public func visit(_ emptyView: EmptyView) {
@@ -40,10 +55,16 @@ public struct WindowsViewRenderer: ViewRenderer {
     }
 
     public func visit(_ objectsTreeView: ObjectsTreeView) {
-        print("\(type(of: self)): visit ObjectsTreeView")
+        print("\(indent())\(type(of: self)): visit ObjectsTreeView")
+        depth += 1
+        objectsTreeView.erasedPresentation.accept(self)
+        depth -= 1
     }
 
     public func visit(_ view: any View) {
-        print("\(type(of: self)): visit \(type(of: view))")
+        print("\(indent())\(type(of: self)): visit \(type(of: view))")
+        depth += 1
+        view.erasedPresentation.accept(self)
+        depth -= 1
     }
 }
